136e1aabe4a079a719b054e5d9a8b971301ba8dd
Maruf Aytekin <aaytekin@gmail.com>
2017-01-08 13:38:38 -0500

    minor style changes

--- src/com/lendapp/algorithms/Sort.java
+++ src/com/lendapp/algorithms/Sort.java
@@ -6,10 +6,10 @@ package com.lendapp.algorithms;
 public abstract class Sort {
 
 
-    public static void swap(Integer[] a, int i, int min) {
+    public static void swap(Integer[] a, int i, int j) {
         int temp = a[i];
-        a[i] = a[min];
-        a[min] = temp;
+        a[i] = a[j];
+        a[j] = temp;
     }
 
 }
